2094cac077a68a941bd279bfbcc571bcf7027ae8
zepedaG06 <jazepeda@uamv.edu.ni>
2026-04-07 19:45:19 -0600

    Clase Estudiante

--- src/Problema_1/Estudiante.java
+++ src/Problema_1/Estudiante.java
@@ -2,7 +2,19 @@ package Problema_1;
 
 public class Estudiante
 {
-
-
-
+    String codigo;
+    String nombre;
+    String carrera;
+    float promedio;
+    public boolean aprobar()
+    {
+        if (promedio >= 70)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
